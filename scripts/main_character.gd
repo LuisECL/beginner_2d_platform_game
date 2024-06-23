eb13d50d17a75b4993018e2aac2e2f42070a3188
LuisECL <lecarcamol@gmail.com>
2024-06-22 22:03:26 -0300

    refactor: MainCharacter -> use RayCast instead of Timer to allow wall jump pushback

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -10,8 +10,8 @@ var was_airbourne = false
 var was_on_wall = false
 var block_wall_slide = false
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var timer = $Timer
 @onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_right = $RayCastRight
 
 func handle_animation(direction: int):
 	# Flip character
@@ -22,7 +22,7 @@ func handle_animation(direction: int):
 
 	# Play animations
 	if not is_on_floor():
-		if is_on_wall():
+		if is_on_wall() and not block_wall_slide:
 			animated_sprite.play("wall_slide")
 		else:
 			animated_sprite.play("jump")
@@ -38,71 +38,84 @@ func get_gravity(velocity_param: Vector2):
 	else: return FALL_GRAVITY
 
 func _physics_process(delta):
-	# Add gravity.
 	if is_on_floor():
+		# Add squash effect
 		if was_airbourne:
 			animated_sprite.scale = Vector2(3.5, 2)
 			was_airbourne = false
 	else:
+		# Add gravity.
 		velocity.y += get_gravity(velocity) * delta
 		was_airbourne = true
-	
+
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("left", "right")
 	# Wall sliding
 	if is_on_wall() and not is_on_floor() and direction != 0 and not block_wall_slide:
-		velocity.y = GRAVITY / 8
+		velocity.y = float(GRAVITY) / 8
 
 	# Animate sprite stretch
 	if velocity.y < 0:
 		animated_sprite.scale = Vector2(2.8, 3.5)
-	
+
 	# Handle movement
 	if was_on_wall:
 		# Allow for pushback when wall jumping
-		pass
+		if direction < 0 and ray_cast_left.is_colliding():
+			pass
+		elif direction > 0 and ray_cast_right.is_colliding():
+			pass
+		else:
+			# To allow movement again once player is far away enough from wall
+			was_on_wall = false
 	else:
 		if direction:
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, 30)
-			
+
 	# Allow player to jump normally even if jumping while bumping a wall
 	if is_on_floor() and is_on_wall() and direction != 0:
 		block_wall_slide = true
 	if velocity.y > 0:
 		block_wall_slide = false
-		
+
 	# To allow push back time when wall jumping
-	if timer.is_stopped() || is_on_floor():
+	if is_on_floor():
 		was_on_wall = false
-	
+
 	# Handle jump
-	if Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y = JUMP_VELOCITY / 4
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		elif is_on_floor() and is_on_wall() and direction != 0:
-			block_wall_slide = true
-		elif is_on_wall() and direction < 0:
-			was_on_wall = true
-			timer.start()
-			velocity.y = WALL_JUMP_VELOCITY
-			velocity.x = SPEED
-		elif is_on_wall() and direction > 0:
-			was_on_wall = true
-			timer.start()
-			velocity.y = WALL_JUMP_VELOCITY
-			velocity.x = -SPEED
+			if is_on_wall() and direction!= 0:
+				# Allow player to jump even if he's running towards a wall
+				block_wall_slide = true
+				velocity.y = JUMP_VELOCITY
+			else:
+				# Jump normally
+				velocity.y = JUMP_VELOCITY
+		elif is_on_wall():
+			if direction < 0:
+				was_on_wall = true
+				#timer.start()
+				velocity.y = WALL_JUMP_VELOCITY
+				velocity.x = SPEED
+			elif direction > 0:
+				was_on_wall = true
+				#timer.start()
+				velocity.y = WALL_JUMP_VELOCITY
+				velocity.x = -SPEED
+		else:
+			# Future for double jump feature
+			pass
+
+	# Fall at a greater speed
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
 
 	move_and_slide()
-	
-	# Handle sprite's animation
 	handle_animation(direction)
+
+	# Handle sprite's squash and stretch effect
 	animated_sprite.scale.x = move_toward(animated_sprite.scale.x, 3, 3 * delta)
 	animated_sprite.scale.y = move_toward(animated_sprite.scale.y, 3, 6 * delta)
-
-
-func _on_timer_timeout():
-	was_on_wall = false # Replace with function body.
